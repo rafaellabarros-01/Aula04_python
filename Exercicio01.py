80350a258dc97e80c73c977040ee6b23ca5e627e
Rafaella <rafaella.barros@ort.org.br>
2026-02-25 15:00:03 -0300

    Finalizei o exercicío 1

---
+++ Exercicio01.py
@@ -0,0 +1,15 @@
+'''
+Crie um programa que leia o salário de um funcionário e mostra na tela o seu salário anual.
+Exemplo:
+Salario_mensal = R$ 2000,00
+Salario_anual = R$ 24000,00
+'''
+#Entrada 
+#Capturando o salário e convertendo para float
+Salario_mensal = float(input('infome seu salario mensal:'))
+
+#processamento
+Salario_anual = Salario_mensal = 12
+
+#Saída
+print("Salário anual: R$", Salario_anual)
